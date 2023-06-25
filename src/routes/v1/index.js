4aaaaed514b73d3354cb58890ddfc4a565e8bd7b
parteek kumar <digitalparteek@gmail.com>
2023-06-25 18:34:07 +0530

    Added code

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -7,6 +7,12 @@ const { BookingController } = require("../../controllers/index");
 const bookingController = new BookingController();
 const router = express.Router();
 
+router.get("/info", (req, res) => {
+  return res.json({
+    message: "Response from routes",
+  });
+});
+
 router.post("/bookings", bookingController.create);
 router.post("/publish", bookingController.sendMessageToQueue);
 // router.patch("/bookings", BookingController.destroy);
